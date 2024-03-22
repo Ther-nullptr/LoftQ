b1a2f287621ce2101564af7327703dfdc8111307
Ther <1329438302@qq.com>
2024-03-22 19:48:46 +0800

    chore: finish memory-save-only mixed operators

--- mixed_layers/mixed_sparse_gated_mlp.py
+++ mixed_layers/mixed_sparse_gated_mlp.py
@@ -1,9 +1,9 @@
 import math
 import torch
 
-class MixedGatedMLPFunc(torch.autograd.Function):
+class MixedSparseGatedMLPFunc(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, x1, w_gate, b_gate, w_gate_lora_a, w_gate_lora_b, w_up, b_up, w_up_lora_a, w_up_lora_b, w_down, b_down, w_down_lora_a, w_down_lora_b, activation_forward, activation_backward):
+    def forward(ctx, x1, w_gate, b_gate, w_gate_lora_a, w_gate_lora_b, w_up, b_up, w_up_lora_a, w_up_lora_b, w_down, b_down, w_down_lora_a, w_down_lora_b, activation_forward, sparsity_ratio, maintain_channels):
         # forward process: gate_proj
         y1_main = x1 @ w_gate + b_gate if b_gate is not None else x1 @ w_gate
         y1_lora_a = x1 @ w_gate_lora_a
@@ -35,57 +35,56 @@ class MixedGatedMLPFunc(torch.autograd.Function):
         y3 = y3_main + y3_lora
 
         # save: x1, y1_lora_a, y1(for soft activations), mask(for hard activations), x2, y2_lora_a
-        if activation_backward == 'relu':
-            mask = y1 < 0
-            if activation_forward != 'relu':
-                xL = torch.relu(xL)
-            ctx.save_for_backward(x1, y1_lora_a, mask, xL, xR, y2_lora_a, x3, y3_lora_a, w_gate, b_gate, w_gate_lora_a, w_gate_lora_b, w_up, b_up, w_up_lora_a, w_up_lora_b, w_down, b_down, w_down_lora_a, w_down_lora_b)
-        else:
-            ctx.save_for_backward(x1, y1_lora_a, y1, xL, xR, y2_lora_a, x3, y3_lora_a, w_gate, b_gate, w_gate_lora_a, w_gate_lora_b, w_up, b_up, w_up_lora_a, w_up_lora_b, w_down, b_down, w_down_lora_a, w_down_lora_b)
+        mask = y1 < 0
+        if activation_forward != 'relu':
+            xL = torch.relu(xL)
 
-        ctx.activation_backward = activation_backward
+        # the pruning of x3 etc. is not urgent, we can implement it in other place
+        zero_count_per_channel = (x3 == 0).sum(dim=-2) # [bs, seq_len, hidden_dim] -> [bs, hidden_dim]
+        actual_maintain_channel = min(int(sparsity_ratio * x3.size(-1)), maintain_channels)
+        # record the top sparsity_ratio channels
+        _, topk_indices = zero_count_per_channel.topk(actual_maintain_channel, dim=-1, largest=False)
+        # delete the sparse channels, and also delete the corresponding x3 channels
+        x3 = x3[:, :, topk_indices]
+        xL = xL[:, :, topk_indices]
+        xR = xR * mask # the xR is sparse version for storage
+        xR = xR[:, :, topk_indices]
+
+        ctx.save_for_backward(x1, y1_lora_a, mask, xL, xR, y2_lora_a, x3, y3_lora_a, w_gate, b_gate, w_gate_lora_a, w_gate_lora_b, w_up, b_up, w_up_lora_a, w_up_lora_b, w_down, b_down, w_down_lora_a, w_down_lora_b)
+        ctx.topk_indices = topk_indices
+        ctx.x3_shape = x3.shape
 
         return y3
     
     @staticmethod
     def backward(ctx, grad_output):
-        if ctx.activation_backward == 'relu':
-            x1, y1_lora_a, mask, xL, xR, y2_lora_a, x3, y3_lora_a, w_gate, b_gate, w_gate_lora_a, w_gate_lora_b, w_up, b_up, w_up_lora_a, w_up_lora_b, w_down, b_down, w_down_lora_a, w_down_lora_b = ctx.saved_tensors
-        else:
-            x1, y1_lora_a, y1, xL, xR, y2_lora_a, x3, y3_lora_a, w_gate, b_gate, w_gate_lora_a, w_gate_lora_b, w_up, b_up, w_up_lora_a, w_up_lora_b, w_down, b_down, w_down_lora_a, w_down_lora_b = ctx.saved_tensors
+        x1, y1_lora_a, mask, xL, xR, y2_lora_a, x3, y3_lora_a, w_gate, b_gate, w_gate_lora_a, w_gate_lora_b, w_up, b_up, w_up_lora_a, w_up_lora_b, w_down, b_down, w_down_lora_a, w_down_lora_b = ctx.saved_tensors
+
+        # convert the x3, xL, xR to the original shape
+        x3 = torch.zeros(ctx.x3_shape, device=x3.device).scatter(-1, ctx.topk_indices.unsqueeze(1).expand(-1, x3.size(1), -1), x3)
+        xL = torch.zeros(ctx.x3_shape, device=x3.device).scatter(-1, ctx.topk_indices.unsqueeze(1).expand(-1, x3.size(1), -1), xL)
+        xR = torch.zeros(ctx.x3_shape, device=x3.device).scatter(-1, ctx.topk_indices.unsqueeze(1).expand(-1, x3.size(1), -1), xR)
 
         # down proj part
-        # d L / d w_down_lora_a = x2.T @ d L / d y2 @ w_down_lora_b.T
+        # d L / d w_down_lora_a = x3.T @ d L / d y3 @ w_down_lora_b.T
         # TODO: x2 maybe sparse
         grad_w_down_lora_a = x3.T @ (grad_output @ w_down_lora_b.T)
-        # d L / d w_down_lora_b = y2_lora_a.T @ d L / d y2
+        # d L / d w_down_lora_b = y3_lora_a.T @ d L / d y3
         grad_w_down_lora_b = y3_lora_a.T @ grad_output
-        # d L / d x2 = d L / d y2 @ w_down.T + d L / d y2 @ w_down_lora_b.T @ w_down_lora_a.T
-        grad_x2 = grad_output @ w_down.T + grad_output @ w_down_lora_b.T @ w_down_lora_a.T
+        # d L / d x3 = d L / d y3 @ w_down.T + d L / d y3 @ w_down_lora_b.T @ w_down_lora_a.T
+        grad_x3 = grad_output @ w_down.T + grad_output @ w_down_lora_b.T @ w_down_lora_a.T
 
         # hadamard product
-        # d L / d xL = d L / d x3 * xR
-        grad_xL = grad_x2 * xR
+        # # d L / d xL = d L / d x3 * xR
+        # grad_xL = grad_x3 * xR
         # d L / d xR = d L / d x3 * xL
-        grad_xR = grad_x2 * xL
-
+        grad_xR = grad_x3 * xL
         # activation part
-        if ctx.activation_backward == 'relu':
-            grad_y1 = grad_xL.clone()
-            grad_y1[mask] = 0
-        elif ctx.activation_backward == 'silu':
-            sigmoid = torch.sigmoid(y1)
-            grad_y1 = sigmoid * (1 + y1 - y1 * sigmoid) * grad_xL
-        elif ctx.activation_backward == 'gelu':
-            gamma = math.sqrt(2 / math.pi)
-            kappa = 0.044715
-            grad_y1 = gamma * (y1 + kappa * y1 ** 3)
-            tanh_y = torch.tanh(y1)
-            grad_y1 = 0.5 * ((1 + tanh_y) + y1 * ((1 - tanh_y ** 2) * gamma * (1 + 3 * kappa * y1 ** 2))) * grad_xL
+        grad_y1 = grad_x3 * xR # notice that the xR there is not the original xR, but the sparse version (xR * mask)
 
         # up proj part
         # d L / d w_up_lora_a = x1.T @ d L / d xL @ w_up_lora_b.T
-        grad_w_up_lora_a = xR.T @ (grad_xL @ w_up_lora_b.T)
+        grad_w_up_lora_a = xR.T @ (grad_xR @ w_up_lora_b.T)
         # d L / d w_up_lora_b = y1_lora_a.T @ d L / d xL
         grad_w_up_lora_b = y2_lora_a.T @ grad_xR
         # d L / d x1 = d L / d xR @ w_up.T + d L / d xR @ w_up_lora_b.T @ w_up_lora_a.T
@@ -99,15 +98,17 @@ class MixedGatedMLPFunc(torch.autograd.Function):
         # d L / d x1 = d L / d xL @ w_gate.T + d L / d xL @ w_gate_lora_b.T @ w_gate_lora_a.T
         grad_x1 += grad_y1 @ w_gate.T + grad_y1 @ w_gate_lora_b.T @ w_gate_lora_a.T
 
-        return grad_x1, None, None, grad_w_gate_lora_a, grad_w_gate_lora_b, None, None, grad_w_up_lora_a, grad_w_up_lora_b, None, None, grad_w_down_lora_a, grad_w_down_lora_b, None, None
+        return grad_x1, None, None, grad_w_gate_lora_a, grad_w_gate_lora_b, None, None, grad_w_up_lora_a, grad_w_up_lora_b, None, None, grad_w_down_lora_a, grad_w_down_lora_b, None, None, None
     
 
-class MixedGatedMLP(torch.nn.Module):
-    def __init__(self, hidden_size, intermediate_size, rank, activation_forward='relu', activation_backward='relu', bias=False):
-        super(MixedGatedMLP, self).__init__()
+class MixedSparseGatedMLP(torch.nn.Module):
+    def __init__(self, hidden_size, intermediate_size, rank, activation_forward='relu', sparsity_ratio=0.5, maintain_channels=10, bias=False):
+        super(MixedSparseGatedMLP, self).__init__()
         self.hidden_size = hidden_size
         self.intermediate_size = intermediate_size
         self.rank = rank
+        self.sparsity_ratio = sparsity_ratio
+        self.maintain_channels = maintain_channels
 
         # linear layers
         self.gate_proj = torch.nn.Linear(self.hidden_size, self.intermediate_size, bias=bias)
@@ -123,10 +124,9 @@ class MixedGatedMLP(torch.nn.Module):
         self.down_proj_lora_b = torch.nn.Linear(self.rank, self.hidden_size, bias=False)
 
         self.activation_forward = activation_forward
-        self.activation_backward = activation_backward
 
     def forward(self, input):
-        return MixedGatedMLPFunc.apply(
+        return MixedSparseGatedMLPFunc.apply(
             input,
             self.gate_proj.weight.T,
             self.gate_proj.bias,
@@ -141,5 +141,6 @@ class MixedGatedMLP(torch.nn.Module):
             self.down_proj_lora_a.weight.T,
             self.down_proj_lora_b.weight.T,
             self.activation_forward,
-            self.activation_backward
+            self.sparsity_ratio,
+            self.maintain_channels
         )
--- mixed_layers/mixed_sparse_traditional_mlp.py
+++ mixed_layers/mixed_sparse_traditional_mlp.py
@@ -1,9 +1,9 @@
 import math
 import torch
 
-class MixedTraditionalMLPFunc(torch.autograd.Function):
+class MixedSparseTraditionalMLPFunc(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, x1, w_up, b_up, w_up_lora_a, w_up_lora_b, w_down, b_down, w_down_lora_a, w_down_lora_b, activation_forward, activation_backward):
+    def forward(ctx, x1, w_up, b_up, w_up_lora_a, w_up_lora_b, w_down, b_down, w_down_lora_a, w_down_lora_b, activation_forward, sparsity_ratio, maintain_channels):
         # forward process: up_proj
         y1_main = x1 @ w_up + b_up if b_up is not None else x1 @ w_up
         y1_lora_a = x1 @ w_up_lora_a
@@ -25,24 +25,31 @@ class MixedTraditionalMLPFunc(torch.autograd.Function):
         y2 = y2_main + y2_lora
 
         # save: x1, y1_lora_a, y1(for soft activations), mask(for hard activations), x2, y2_lora_a
-        if activation_backward == 'relu':
-            mask = y1 < 0
-            if activation_forward != 'relu':
-                x2 = torch.relu(x2) # cache the sparse version of x2
-            ctx.save_for_backward(x1, y1_lora_a, mask, x2, y2_lora_a, w_up, b_up, w_up_lora_a, w_up_lora_b, w_down, b_down, w_down_lora_a, w_down_lora_b)
-        else:
-            ctx.save_for_backward(x1, y1_lora_a, y1, x2, y2_lora_a, w_up, b_up, w_up_lora_a, w_up_lora_b, w_down, b_down, w_down_lora_a, w_down_lora_b)
+        mask = y1 < 0
+        if activation_forward != 'relu':
+            x2 = torch.relu(x2) # cache the sparse version of x2
 
-        ctx.activation_backward = activation_backward
+        #! notice that: the pruning of x2 etc. is not urgent, we can implement it in other place
+        # for x2: delete useless channels(mostly 0)
+        zero_counts_per_channel = (x2 == 0).sum(dim=-2) # [bs, seq_len, hidden_dim] -> [bs, hidden_dim]
+        actual_maintain_channel = min(int(sparsity_ratio * x2.size(-1)), maintain_channels)
+        # record the top sparsity_ratio channels
+        _, topk_indices = zero_counts_per_channel.topk(actual_maintain_channel, dim=-1, largest=False)
+        # delete the sparse channels, and also delete the corresponding x2 channels
+        x2 = x2[:, :, topk_indices]
+
+        ctx.save_for_backward(x1, y1_lora_a, mask, x2, y2_lora_a, w_up, b_up, w_up_lora_a, w_up_lora_b, w_down, b_down, w_down_lora_a, w_down_lora_b)
+        ctx.topk_indices = topk_indices
+        ctx.x2_shape = x2.shape
 
         return y2
 
     @staticmethod
     def backward(ctx, grad_output):
-        if ctx.activation_backward == 'relu':
-            x1, y1_lora_a, mask, x2, y2_lora_a, w_up, b_up, w_up_lora_a, w_up_lora_b, w_down, b_down, w_down_lora_a, w_down_lora_b = ctx.saved_tensors
-        else:
-            x1, y1_lora_a, y1, x2, y2_lora_a, w_up, b_up, w_up_lora_a, w_up_lora_b, w_down, b_down, w_down_lora_a, w_down_lora_b = ctx.saved_tensors
+        x1, y1_lora_a, mask, x2, y2_lora_a, w_up, b_up, w_up_lora_a, w_up_lora_b, w_down, b_down, w_down_lora_a, w_down_lora_b = ctx.saved_tensors
+
+        # convert the x2 to the original shape
+        x2 = torch.zeros(ctx.x2_shape, device=x2.device).scatter(-1, ctx.topk_indices.unsqueeze(1).expand(-1, x2.size(1), -1), x2)
 
         # down proj part
         # d L / d w_down_lora_a = x2.T @ d L / d y2 @ w_down_lora_b.T
@@ -54,18 +61,8 @@ class MixedTraditionalMLPFunc(torch.autograd.Function):
         grad_x2 = grad_output @ w_down.T + grad_output @ w_down_lora_b.T @ w_down_lora_a.T
 
         # activation part
-        if ctx.activation_backward == 'relu':
-            grad_y1 = grad_x2.clone()
-            grad_y1[mask] = 0
-        elif ctx.activation_backward == 'silu':
-            sigmoid = torch.sigmoid(y1)
-            grad_y1 = grad_x2 * sigmoid * (1 + y1 - y1 * sigmoid)
-        elif ctx.activation_backward == 'gelu':
-            gamma = math.sqrt(2 / math.pi)
-            kappa = 0.044715
-            y1_extra = gamma * (y1 + kappa * y1 ** 3)
-            tanh_y = torch.tanh(y1_extra)
-            grad_y1 = grad_x2 * 0.5 * ( (1 + tanh_y) + y1 * ( (1 - tanh_y ** 2) * gamma * (1 + 3 * kappa * y1 ** 2) ) )
+        grad_y1 = grad_x2.clone()
+        grad_y1[mask] = 0
 
         # up proj part
         # d L / d w_up_lora_a = x1.T @ d L / d y1 @ w_up_lora_b.T
@@ -76,15 +73,17 @@ class MixedTraditionalMLPFunc(torch.autograd.Function):
         grad_x1 = grad_y1 @ w_up.T + grad_y1 @ w_up_lora_b.T @ w_up_lora_a.T
 
         # TODO: add bias support 
-        return grad_x1, None, None, grad_w_up_lora_a, grad_w_up_lora_b, grad_x2, None, None, grad_w_down_lora_a, grad_w_down_lora_b, None, None 
+        return grad_x1, None, None, grad_w_up_lora_a, grad_w_up_lora_b, grad_x2, None, None, grad_w_down_lora_a, grad_w_down_lora_b, None, None, None
 
 
-class MixedTraditionalMLP(torch.nn.Module):
-    def __init__(self, hidden_size, intermediate_size, rank, activation_forward='relu', activation_backward='relu', bias=False):
-        super(MixedTraditionalMLP, self).__init__()
+class MixedSparseTraditionalMLP(torch.nn.Module):
+    def __init__(self, hidden_size, intermediate_size, rank, activation_forward='relu', sparsity_ratio=0.5, maintain_channels=10, bias=False):
+        super(MixedSparseTraditionalMLP, self).__init__()
         self.hidden_size = hidden_size
         self.intermediate_size = intermediate_size
         self.rank = rank
+        self.sparsity_ratio = sparsity_ratio
+        self.maintain_channels = maintain_channels
 
         # linear layers
         self.up_proj = torch.nn.Linear(self.hidden_size, self.intermediate_size, bias=bias)
@@ -95,13 +94,12 @@ class MixedTraditionalMLP(torch.nn.Module):
         self.down_proj_lora_a = torch.nn.Linear(self.intermediate_size, self.rank, bias=False)
         self.down_proj_lora_b = torch.nn.Linear(self.rank, self.hidden_size, bias=False)
 
-        # activation function method. Now support: ReLU, SiLU, GELU
+        # activation function method. Now support: ReLU, SiLU, GELU. Notice that default activation_backward is relu
         self.activation_forward = activation_forward
-        self.activation_backward = activation_backward
 
     def forward(self, x):
         #! Notice we use equation y = xW + b; instead of default y = xW^T + b
-        return MixedTraditionalMLPFunc.apply(
+        return MixedSparseTraditionalMLPFunc.apply(
             x, 
             self.up_proj.weight.T, 
             self.up_proj.bias,
@@ -112,5 +110,6 @@ class MixedTraditionalMLP(torch.nn.Module):
             self.down_proj_lora_a.weight.T,
             self.down_proj_lora_b.weight.T,
             self.activation_forward,
-            self.activation_backward,
+            self.sparsity_ratio,
+            self.maintain_channels
         )
